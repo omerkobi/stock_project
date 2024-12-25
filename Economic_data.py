0a75502747fe85c11500c4bf467c8d27b5af6809
omerkobi <omerkobi9@gmail.com>
2024-12-25 15:52:16 +0200

    calculated the presentage change

--- Economic_data.py
+++ Economic_data.py
@@ -5,14 +5,28 @@ import json
 
 
 
+
 fred_key = '25af470e22d07300c84e19895ed91600'
 data_type = "CPIAUCSL"
 start_data_date = '2000-01-01'
 url = f'https://api.stlouisfed.org/fred/series/observations?series_id={data_type}&api_key={fred_key}&file_type=json&observation_start={start_data_date}'
 fred = re.get(url)
-data = fred.json() # getting a dict object
-observations = data.get("observations", []) # extracting the data which is under the observation key
-df = pd.DataFrame(observations) # put the data in a data frame
-df['date'] = pd.to_datetime(df['date'])
-df['value'] = df['value'].astype(float) # valu is str convert to float in order to calculate the % change
-print(df.tail(20))
+df = pd.DataFrame()
+if fred.status_code == 200:
+    data = fred.json() # getting a dict object
+    observations = data.get("observations", []) # extracting the data which is under the observation key
+    df = pd.DataFrame(observations) # put the data in a data frame
+    df['date'] = pd.to_datetime(df['date'])
+    df['value'] = df['value'].astype(float) # valu is str convert to float in order to calculate the % change
+#print(df.tail(20))
+    df['monthly_change'] = df['value'].pct_change() * 100
+    df['monthly_change'] = df['monthly_change'].apply(lambda x: f"{x:.1f}%" if pd.notnull(x) else "NaN")
+    df['yearly_change'] = (df['value'] / df['value'].shift(12) - 1) * 100 # calculate the yearly change
+    df['yearly_change'] = df['yearly_change'].apply(lambda x: f"{x:.1f}%" if pd.notnull(x) else "NaN")
+#print(df.tail(20))
+else :
+    print('error type: ', fred.status_code)
+
+print(df.tail(10))
+
+
--- main1.py
+++ main1.py
@@ -19,6 +19,7 @@ st.markdown("""
 st.radio("what kind of data would ypu like to see",['economic date','stock data'])
 
 df=sns.load_dataset("penguins")
+plot_choice = st.radio("choose what would you like to see",['economic data','stock data']) # telling the user to pick
 fig, axe = plt.subplots() # creat a new figure, get the axes object
 sns.scatterplot(data=df,x= "flipper_length_mm", y= "bill_length_mm", hue="species", ax = axe)
 st.pyplot(fig)
