f9968bd243b3a86153ecb638ac22f562ddf7f81c
omerkobi <omerkobi9@gmail.com>
2024-12-29 14:51:26 +0200

    Add another graph to display

--- main1.py
+++ main1.py
@@ -2,6 +2,7 @@ import streamlit as st
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
+import plotly.express as px
 import Economic_data as e
 import stock_data as s
 fred_key = '25af470e22d07300c84e19895ed91600'
@@ -57,14 +58,20 @@ if search_term:
                 sign =["","%","%"]
                 plot_choice = st.radio("choose what would you like to see",kind_of_graph)
                 #plot_choice = st.radio("choose what would you like to see",['total value graph', 'presentage change graph'])  # telling the user to pick
-                fig, axe = plt.subplots()  # creat a new figure, get the axes object
-                for pick, typ,sig in zip(kind_of_graph,type_,sign):
+                for pick, typ,sig in zip(kind_of_graph,type_,sign): # iterate over the 3 list to display the desiered data
 
                     if plot_choice == pick:
-                        sns.lineplot(data=df, x="date", y=typ, ax=axe)
-                        plt.xticks(ticks=df['date'][::4],rotation=90,fontsize=5)
-                        plt.yticks(fontsize=6)
-                        st.pyplot(fig)
+                        graph_choice = st.radio("line graph or scatterplot ?",['line','scatter'])
+                        if graph_choice == 'line':
+                            fig, axe = plt.subplots()  # creat a new figure, get the axes object
+                            sns.lineplot(data=df, x="date", y=typ, ax=axe)
+                            plt.xticks(ticks=df['date'][::4],rotation=90,fontsize=5)
+                            plt.yticks(fontsize=6)
+                            st.pyplot(fig)
+                        elif graph_choice == 'scatter':
+                            fig = px.scatter(df,x= 'date', y=typ, title= 'Data by Date')
+                            st.plotly_chart(fig)
+
                         plot_choice2 = st.radio("Any specific value to fetch? "
                                                 "(its better to display the presentage change and not the absolut value) "
                                                 ,['Max value', 'Min value','Average value','median value','pick a specific date'])
@@ -84,14 +91,7 @@ if search_term:
                         if plot_choice2 == 'median value':
                             median_val = df[typ].median()
                             st.text(f'{median_val}{sig}')
-                    #if plot_choice2 == 'Min value':
-                     #   st.text(df['value'].min())
-                    #if plot_choice2 == 'Max value':
-                     #   st.text(df['value'].max())
-                    #if plot_choice2 == 'Average value':
-                    #    st.text(df['value'].mean())
-                    #if plot_choice2 == 'median value':
-                    #    st.text(df['value'].median())
+
 
                         if plot_choice2 == 'pick a specific date': # to display a specific date
                             selected_year = st.selectbox("Select Year:", sorted(df['year'].unique()))
@@ -103,38 +103,11 @@ if search_term:
                             else:
                                 st.write("No data available for the selected month and year.")
 
-                #if plot_choice == 'presentage change graph':
-                 #   sns.lineplot(data=df, x="date", y='change_from_last', ax=axe)
-                 #   plt.xticks(ticks=df['date'][::4],rotation=90,fontsize=5)
-                 #   plt.yticks(fontsize=6)
-                 #   st.pyplot(fig)
-                 #   plot_choice2 = st.radio("Any specific value to fetch? "
-                  #                          "(its better to display the presentage change and not the absolut value) ",
-                  #                          ['Max value', 'Min value', 'Average value','median value','pick a specific date'])
-                   # if plot_choice2 == 'Min value':
-                   #     min_val = df['change_from_last'].min()
-                   #     st.text(f'{min_val}%')
-                   # if plot_choice2 == 'Max value':
-                   #     max_val = df['change_from_last'].max()
-                   #     st.text(f'{max_val}%')
-                   # if plot_choice2 == 'Average value':
-                   #     average_val = df['change_from_last'].mean()
-                   #     st.text(f'{average_val}%')
-                   # if plot_choice2 == 'median value':
-                   #     median_val = df['change_from_last'].median()
-                   #     st.text(f'{median_val}%')
 
-                    #if plot_choice2 == 'pick a specific date':
-                     #   selected_year = st.selectbox("Select Year:", sorted(df['year'].unique()))
-                      #  selected_month = st.selectbox("Select Month:", sorted(df['month'].unique()))
-                       # specific_date = df[(df['year'] == selected_year) & (df['month'] == selected_month)]
-                        #if not specific_date.empty:
-                         #   st.write("Values for the selected date range:")
-                          #  st.write(specific_date[['date', 'change_from_last']])
-                        #else:
-                        #    st.write("No data available for the selected month and year.")
     else:
         st.write("No data available for your search, try another keyword.")
+
+###### Stock DATA : ###### this section is to provide a desire stock data
 st.title('Stock data')
 search_stock = st.text_input("Enter the ticker of the stock you are looking for :", "")
 if search_stock:
